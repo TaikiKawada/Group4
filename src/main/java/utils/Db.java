df00d6a11c381fb5579c1bd8bbfc2c5641d4129a
WINDOWS-70BP582\User <taiki_kawada@trustsystem.co,jp>
2025-06-09 16:26:13 +0900

    init

---
+++ src/main/java/utils/Db.java
@@ -0,0 +1,23 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Db {
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        String url = "jdbc:mariadb://localhost:3306/todo";
+        String user = "root";
+        String password = "root";
+
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public static void close(Connection c) throws SQLException {
+        if (c != null && !c.isClosed()) {
+            c.close();
+        }
+    }
+}
