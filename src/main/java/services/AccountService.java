38f74e03cedcd654df2fb88dd0ec089ddd97fc31
WINDOWS-VA8JRUR\User <ren_goda@trustsystem.co.jp>
2025-06-10 11:48:52 +0900

    郷田2回目

--- src/main/java/services/AccountService.java
+++ src/main/java/services/AccountService.java
@@ -1,5 +1,32 @@
 package services;
 
-public class AccountService {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import javax.naming.NamingException;
+
+import utils.Db;
+
+public class AccountService {
+	public void signup(User obj) {
+		String sql = "insert into user (name, mail, pass) values (?, ?, ?)";
+		
+		try(
+			Connection con = Db.open();
+			PreparedStatement ps = con.prepareStatement(sql);
+			){
+			
+			ps.setString(1, obj.getName());
+			ps.setString(2, obj.getMail());
+			ps.setString(3, obj.getPass());
+			
+			ps.executeUpdate();
+			
+		}catch(SQLException e){
+			e.printStackTrace();
+		}catch(NamingException e){
+			e.printStackTrace();
+		}
+	}
 }
