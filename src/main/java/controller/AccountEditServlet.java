ca8787e5a3ca603620a3f9f38567fee3e23792b9
WINDOWS-VA8JRUR\User <ren_goda@trustsystem.co.jp>
2025-06-13 13:43:54 +0900

    郷田：アカウント検索機能実装

---
+++ src/main/java/controller/AccountEditServlet.java
@@ -0,0 +1,64 @@
+package controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import DAO.AccountDao;
+import DTO.AccountDto;
+
+/**
+ * Servlet implementation class AccountEditServlet
+ */
+@WebServlet("/account/edit.html")
+public class AccountEditServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AccountEditServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// 更新するアカウントのaccount_idを取得
+		int accountId = Integer.parseInt(request.getParameter("account_id"));
+		
+		// 更新するアカウントの情報を取得
+		AccountDto account = new AccountDao().findById(accountId);
+		
+		// 権限のチェック
+		int auth = account.getAuth();
+		
+		boolean hasNoneAuth = (auth == 0);
+		boolean hasSalesAuth = (auth & 1) != 0;
+		boolean hasAccountAuth = (auth & 2) != 0;
+		
+		// アカウント情報をセット
+		request.setAttribute("hasNoneAuth", hasNoneAuth);
+		request.setAttribute("hasSalesAuth", hasSalesAuth);
+		request.setAttribute("hasAccountAuth", hasAccountAuth);		
+		request.setAttribute("account", account);
+		// 更新画面に遷移
+		request.getRequestDispatcher("/account_edit.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
